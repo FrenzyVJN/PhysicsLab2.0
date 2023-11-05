1c7efb98d6c2e1561c3a8fbecba5c5855a1a28f0
Vijayan Sankar <svijayan01@gmail.com>
2023-11-05 14:43:12 +0530

    v2

--- physicslab2.0/src/pclab/main.tsx
+++ physicslab2.0/src/pclab/main.tsx
@@ -1,4 +1,7 @@
 "use client"
+import React, { useState } from 'react';
+
+
 
 function Slider({label}: {label: string}) {
     return(
@@ -19,20 +22,43 @@ function Display({value}: {value:Number}) {
         </div>
     )
 }
+
+function Light({color}: {color:string}) {
+    const customStyles = {
+        backgroundColor: color, // Use the color prop as the background color
+      };
+    return(
+        <div>
+
+            <div
+            className="rounded-full px-15 py-16" style={customStyles}>
+            </div>
+        </div>
+        )
+}
+
 export default function laboratory() {
-    return ( 
+    const [lightColor, setLightColor] = useState(false); 
+    const [waveColor, setWaveColor] = useState('white');
+    const handleLightButtonClick = () => {
+        setLightColor(!lightColor);
+    }
+    const handleLightButtonClick1 = () => {
+        setWaveColor('green');
+      };
+    return (
     <main className='flex w-screen h-screen flex-col items-center bg-gradient-to-b from-blue-950 to-blue-400'>
-        <header className="flex fixed w-full items-center bg-gradient-to-r from-blue-700 to-blue-400 text-white p-5">
+        <header className="flex fixed w-full items-center bg-gradient-to-tr from-blue-700 to-blue-400 text-white p-3">
             <img className="flex h-16 w-1/8" src="https://media.discordapp.net/attachments/1164584907192938657/1170592901303124018/image_8.png?ex=65599a98&is=65472598&hm=74797897c1f89f538f522779938c1aa26fc4ddec605dc866cd0ac2ca1ae1e129&=&width=1440&height=398" alt="SNU Chennai" />
-            <h1 className="flex items-end h-1/3 ml-auto font-bold text-3xl">Physics Virtual Lab</h1>
+            <h1 className="flex items-end h-1/3 mx-auto px-48 font-bold text-3xl">Physics Virtual Lab</h1>
             <a href='/' className='flex ml-auto mr-12 font-bold text-white text-2xl'>Home</a>
         </header>
-        <div className='flex my-auto justify-center flex-col items-center border border-black px-20 rounded-2xl bg-gradient-to-t from-blue-950 to-blue-500 h-2/3 w-1/2'>
-            <h1 className=" text-3xl text-white font-sans font-semibold py-1">Planck’s Constant  Experimental Setup</h1>
+        <div className='flex my-auto mb-auto justify-center flex-col items-center border border-black px-20 rounded-2xl bg-gradient-to-t from-blue-950 to-blue-500 h-2/3 w-1/2'>
+            <h1 className=" text-3xl text-white font-sans font-semibold py-1">Planck’s Constant Setup</h1>
             <div className="flex h-fit w-full justify-center items-center px-15 border rounded-xl border-blue-200">
-                <div className="h-4/5 w-1/3 bg-black rounded-full ml-3 mr-auto"></div>
+                <div className="h-4/5 w-1/3 ml-3 mr-auto rounded-full" id="frame"><Light color={waveColor}/></div>
                 <div className="flex h-1/2 w-full justify-center px-15 py-20"></div>
-                <div className="h-4/5 w-1/3 bg-yellow-300 rounded-full mr-3 ml-auto"></div>
+                <div className="h-4/5 w-1/3 rounded-full mr-3 ml-auto" id="light"><Light color={lightColor?'yellow':'white'}/></div>
             </div>
             <div className='inline-flex justify-center items-center'>
                 <div className='px-10'>
@@ -53,6 +79,8 @@ export default function laboratory() {
                 <button className='bg-blue-500 px-4 text-white py-1 border border-black rounded-md mx-3'>635</button>
                 <button className='bg-blue-500 text-white px-4 py-1 border border-black rounded-md mx-3'>460</button>
             </div>    
+            <button className='bg-blue-500 text-white px-4 py-1 border border-black rounded-md mx-3' onClick={handleLightButtonClick}>Light</button>
+            
     </div>
     </main> 
     );

64aef3f6c064b405b6743aa614db50cd88662395
Vijayan Sankar <svijayan01@gmail.com>
2023-11-05 11:07:09 +0530

    v1

--- physicslab2.0/src/pclab/main.tsx
+++ physicslab2.0/src/pclab/main.tsx
@@ -1,6 +1,5 @@
 "use client"
-{/*
-import { useRef, forwardRef } from 'react'
+
 function Slider({label}: {label: string}) {
     return(
         <form className='flex flex-col'>
@@ -9,15 +8,6 @@ function Slider({label}: {label: string}) {
         </form>
     )
 }
-function Toggle({label}: {label: string}) {
-    return(
-    <label className="relative inline-flex mt-auto items-center cursor-pointer outline-none">
-        <input type="checkbox" value="" className="sr-only peer" ></input>
-        <div className="w-10 h-6 bg-gray-200 peer-focus:outline-none peer-focus:4 peer-focus:blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 outline-none"></div>
-    </label>
-    )
-}
-*/}
 
 function Display({value}: {value:Number}) {
     return(

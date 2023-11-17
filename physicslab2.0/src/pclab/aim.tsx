f2d1d34322a4e56d86b621b4dd7d4867b19da399
Vijayan Sankar <svijayan01@gmail.com>
2023-11-17 22:03:30 +0530

    minor fixes v1

--- physicslab2.0/src/pclab/aim.tsx
+++ physicslab2.0/src/pclab/aim.tsx
@@ -5,7 +5,7 @@ function Aim(props : any) {
         <div className="popup">
             <div className="popup-inner text-black bg-white flex mx-auto rounded-lg">
                 {props.children}
-                <button className="flex ml-auto text-white" onClick={() => props.setTrigger(false)}><AiOutlineClose/></button>
+                <button className="flex ml-auto text-black" onClick={() => props.setTrigger(false)}><AiOutlineClose/></button>
             </div>
         </div>
      ) : "";
--- physicslab2.0/src/pclab/main.tsx
+++ physicslab2.0/src/pclab/main.tsx
@@ -23,31 +23,6 @@ function Display({value, label}: {value:Number ; label:string}) {
     )
 }
 
-function Slider({ label, value, onChange }: { label: string; value: number; onChange: (newValue: number) => void }) {
-    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const newValue = parseInt(event.target.value, 10); // Parse the input value as an integer
-      onChange(newValue); // Call the onChange function with the new value
-    };
-  
-    return (
-      <form className="flex flex-col">
-        <label className="text-white font-thin" htmlFor={label}>{label}</label>
-        <input className="accent-white active:accent-slate-400" type="range" id={label} name={label} min="-50" max="50" step="1" value={value} onChange={handleSliderChange} />
-      </form>
-    );
-  }
-  function Slider1({ label, value, onChange }: { label: string; value: number; onChange: (newValue: number) => void }) {
-    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = parseInt(event.target.value, 10); // Parse the input value as an integer
-      onChange(newValue); // Call the onChange function with the new value
-    };
-    return (
-      <form className="flex flex-col">
-        <label className="text-white font-thin" htmlFor={label}>{label}</label>
-        <input className="accent-white active:accent-slate-400" type="range" id={label} name={label} min="0" max="3" value={value} onChange={handleSliderChange} />
-      </form>
-    );
-  }
 function Light({color}: {color:string}) {
     const customStyles = {
         backgroundColor: color,
@@ -62,7 +37,6 @@ function Light({color}: {color:string}) {
         )
 }
 
-
 export default function laboratory() {
     const [buttonProp1, setButton1Prop] = useState(false);
     const [buttonProp2, setButton2Prop] = useState(false);
@@ -70,8 +44,6 @@ export default function laboratory() {
     const [waveLength, setWaveLength] = useState(0);
     const [waveColor, setWaveColor] = useState('white');
     const [userVoltage, setUserVoltage] = useState(0);
-    const [lightIntensity, setLightIntensity] = useState(0);
-    const [currentMultiplier, setCurrentMultiplier] = useState(0);
 
     const handleLightButtonClick = () => {
         setLightColor(!lightColor);
@@ -108,14 +80,7 @@ export default function laboratory() {
         setUserVoltage(-0.30)
 
     }
-    const handleLightIntensity = (newValue : number) => {
-        console.log("Light Intensity - ",newValue);
-        setLightIntensity(newValue);
-    }
-    const handleCurrentMultiplier = (newValue : number) => {
-        console.log("Current multiplier - "+newValue);
-        setCurrentMultiplier(newValue);
-    }
+
     return (
     <main className='flex w-screen h-screen flex-col items-center bg-gradient-to-b from-blue-950 to-blue-400'>
         <header className="flex fixed w-full items-center bg-gradient-to-tr from-blue-700 to-blue-400 text-white p-3">
@@ -126,7 +91,7 @@ export default function laboratory() {
         </header>
         <div className='flex my-auto mb-1/2 justify-center flex-col items-center border border-black w-1/2 px-16 shadow-lg py-3 rounded-2xl bg-gradient-to-t from-blue-950 to-blue-500'>
             <h1 className=" mt-4 text-3xl font-extrabold tracking-[-0.04em] text-white sm:text-3xl sm:leading-[3.5rem]">Planck’s Constant Setup</h1>
-            <div className="flex h-fit w-full justify-center items-center px-15 border rounded-xl border-blue-200">
+            <div className="flex h-fit w-full justify-center items-center px-15 border rounded-xl mb-5 border-blue-200">
                 <div className="h-4/5 w-1/3 ml-3 mr-auto rounded-full" id="frame"><Light color={waveColor}/></div>
                 <div className="flex h-1/2 w-full justify-center px-15 py-20"></div>
                 <div className="h-4/5 w-1/3 rounded-full mr-3 ml-auto" id="light"><Light color={lightColor?'yellow':'white'}/></div>
@@ -145,13 +110,11 @@ export default function laboratory() {
                     <Aim trigger={buttonProp1} setTrigger={setButton1Prop}>
                         <h3>Calculate (i) Planck’s constant and (ii) work function of the metal using photoelectric effect</h3>
                     </Aim>
-                    <button className='flex mx-auto bg-inherit border border-white text-white text-center py-1 px-2' onClick={() => setButton2Prop(true)}>Formula</button>
+                    <button className='flex mx-auto bg-inherit border mt-2 border-white text-white text-center py-1 px-2' onClick={() => setButton2Prop(true)}>Formula</button>
                     <Formulas trigger={buttonProp2} setTrigger={setButton2Prop}>
                         <h3>Planck's Constant = (eV)/(λ)</h3>
                         <h3>Work Function = (eV)-(φ)</h3>
                     </Formulas>
-                    <Slider label="Light Intensity" value={lightIntensity} onChange={handleLightIntensity}/>
-                    <Slider1 label="Current Multiplier" value={currentMultiplier} onChange={handleCurrentMultiplier} />
                     {/*<Slider label="Voltage Adjustor" value={userVoltage} />*/}
                 </div>
                 

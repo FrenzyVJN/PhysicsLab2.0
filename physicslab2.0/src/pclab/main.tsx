afb47d0016ff98fd5536a14d6ff32ba9c2e5e751
Vijayan Sankar <svijayan01@gmail.com>
2023-11-22 09:37:40 +0530

    button fixes v2

--- physicslab2.0/src/pclab/main.tsx
+++ physicslab2.0/src/pclab/main.tsx
@@ -85,13 +85,25 @@ export default function laboratory() {
     <main className='flex w-screen h-screen flex-col items-center bg-gradient-to-b from-blue-950 to-blue-400'>
         <header className="flex fixed w-full items-center bg-gradient-to-tr from-blue-700 to-blue-400 text-white p-3">
             <img className="flex h-16 w-1/8" src="https://media.discordapp.net/attachments/1164584907192938657/1170592901303124018/image_8.png?ex=65599a98&is=65472598&hm=74797897c1f89f538f522779938c1aa26fc4ddec605dc866cd0ac2ca1ae1e129&=&width=1440&height=398" alt="SNU Chennai" />
-            <h1 className="flex text-3xl font-extrabold tracking-[-0.05em] text-white sm:text-5xl sm:leading-[3.5rem] ml-auto">Physics Virtual Lab</h1>
+            <h1 className="flex text-3xl font-bold tracking-[-0.05em] text-white sm:text-5xl sm:leading-[3.5rem] ml-auto">Physics Virtual Lab</h1>
             <a href='/' className='flex ml-auto font-bold text-white text-2xl'>Home</a>
             <a href='/' className='flex font-bold mr-3 ml-3 text-white text-2xl'>Simulation</a>
         </header>
-        <div className='flex my-auto mb-1/2 justify-center flex-col items-center border border-black w-1/2 px-16 shadow-lg py-3 rounded-2xl bg-gradient-to-t from-blue-950 to-blue-500'>
-            <h1 className=" mt-4 text-3xl font-extrabold tracking-[-0.04em] text-white sm:text-3xl sm:leading-[3.5rem]">Planck’s Constant Setup</h1>
-            <div className="flex h-fit w-full justify-center items-center px-15 border rounded-xl mb-5 border-blue-200">
+        <div className='flex mt-auto'>
+            <button className='flex mx-auto bg-inherit border mb-4 border-white text-white text-center py-1 px-2' onClick={() => setButton1Prop(true)}>Aim</button>
+            <Aim trigger={buttonProp1} setTrigger={setButton1Prop}>
+                <h3>Calculate (i) Planck’s constant and (ii) work function of the metal using photoelectric effect</h3>
+            </Aim>
+            <button className='flex mx-auto bg-inherit border mb-4 border-white text-white text-center py-1 px-2' onClick={() => setButton2Prop(true)}>Formula</button>
+            <Formulas trigger={buttonProp2} setTrigger={setButton2Prop}>
+                <h3>Planck's Constant = (eV)/(λ)</h3>
+                <h3>Work Function = (eV)-(φ)</h3>
+            </Formulas>
+            {/*<Slider label="Voltage Adjustor" value={userVoltage} />*/}
+        </div>
+        <div className='flex mb-auto mb-1/2 justify-center flex-col items-center border border-black w-1/2 px-16 shadow-lg py-3 rounded-2xl bg-gradient-to-t from-blue-950 to-blue-500'>
+            <h1 className=" mt-4 text-3xl font-bold tracking-[-0.04em] text-white sm:text-3xl sm:leading-[3.5rem]">Planck’s Constant Setup</h1>
+            <div className="flex h-fit w-full justify-center items-center px-15 border-4 rounded-xl mb-5 border-blue-400">
                 <div className="h-4/5 w-1/3 ml-3 mr-auto rounded-full" id="frame"><Light color={waveColor}/></div>
                 <div className="flex h-1/2 w-full justify-center px-15 py-20"></div>
                 <div className="h-4/5 w-1/3 rounded-full mr-3 ml-auto" id="light"><Light color={lightColor?'yellow':'white'}/></div>
@@ -105,18 +117,7 @@ export default function laboratory() {
                 <div className='px-4'>
                     <Display value={(userVoltage)} label={"Voltage"}/>
                 </div>
-                <div>
-                    <button className='flex flex-col mx-auto bg-inherit border border-white text-white text-center py-1 px-2' onClick={() => setButton1Prop(true)}>Aim</button>
-                    <Aim trigger={buttonProp1} setTrigger={setButton1Prop}>
-                        <h3>Calculate (i) Planck’s constant and (ii) work function of the metal using photoelectric effect</h3>
-                    </Aim>
-                    <button className='flex mx-auto bg-inherit border mt-2 border-white text-white text-center py-1 px-2' onClick={() => setButton2Prop(true)}>Formula</button>
-                    <Formulas trigger={buttonProp2} setTrigger={setButton2Prop}>
-                        <h3>Planck's Constant = (eV)/(λ)</h3>
-                        <h3>Work Function = (eV)-(φ)</h3>
-                    </Formulas>
-                    {/*<Slider label="Voltage Adjustor" value={userVoltage} />*/}
-                </div>
+
                 
             </div>
             <div className='inline-flex justify-center items-center py-2'>
